b7741a7a9768ff86ca3216f29d5d5bfdfbb86744
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2024-03-18 13:14:01 +0800

    23MJ41.py

--- 23MJ41.py
+++ 23MJ41.py
@@ -1,137 +1,154 @@
-Animals = ['' for i in range(10)]
-Animals[0] = 'horse'
-Animals[1] = 'lion'
-Animals[2] = 'rabbit'
-Animals[3] = 'mouse'
-Animals[4] = 'bird'
-Animals[5] = 'deer'
-Animals[6] = 'whale'
-Animals[7] = 'elephant'
-Animals[8] = 'kangaroo'
-Animals[9] = 'tiger'
-
-
-def sortdescending():
-    arrayLength = len(Animals)
-    for X in range(arrayLength-1):
-        for Y in range(arrayLength-X-1):
-            if Animals[Y] < Animals[Y+1]:
-                Temp = Animals[Y]
-                Animals[Y] = Animals[Y+1]
-                Animals[Y+1] = Temp
+DataArray = ['' for i in range(25)]
 #main
-sortdescending()
-for i in range(10):
-    print(Animals[i])
+DataFile = 'C:/Users/N129ALS22030/Desktop/Data.txt'
+File = open(DataFile, 'r')
+for i in range(25):
+    read = File.readline()
+    DataArray[i] = read
+File.close()
+def PrintArray(DataArray):
+    data = ''
+    for i in range(25):
+        data = data + DataArray[i]
+    print(data)
+PrintArray(DataArray)
+def LinearSearch(DataArray, searchval):
+    count = 0
+    for i in range(25):
+        if DataArray[i] == searchval:
+            count = count + 1
+    return count
+#main
+num = int(input('enter number between 0-100: '))
+while num < 0 or num > 100:
+    num = int(input('invalid range, please enter number between 0-100: '))
+Count = LinearSearch(DataArray,num)
+print('The number',num,'is found',Count,'times.')
 
-class SaleData():
-    def __init__(self,ID, Quantity):
-        self.ID = ID
-        self.Quantity = Quantity
-CircularQueue = []
-for i in range(5):
-    Temp = SaleData('',-1)
-    CircularQueue.append(Temp)
-Head = 0
-Tail = 0
-NumberOfItems = 0
-#----------------------------------------------------------
-def Enqueue(newrecord):
-    global Head,Tail,NumberOfItems,CircularQueue
-    if NumberOfItems >= 5:
-        print('queue is full')
-        return -1
+Animal = ['' for i in range (20)]
+Colour = ['' for j in range(10)]
+AnimalTopPointer = 0
+ColourTopPointer = 0
+def PushAnimal(DataToPush):
+    global AnimalTopPointer
+    if AnimalTopPointer == 20:
+        return False
     else:
-        Tail = Tail + 1
-        if Tail > 4:
-            Tail = 0
-        CircularQueue[Tail] = newrecord
-        NumberOfItems = NumberOfItems + 1
-        return 1
-def Dequeue():
-    global Head, Tail, NumberOfItems, CircularQueue
-    if NumberOfItems <= 0 :
-        print('Queue is empty')
+        Animal[AnimalTopPointer] = DataToPush
+        AnimalTopPointer = AnimalTopPointer + 1
+        return True
+def PopAnimal():
+    global AnimalTopPointer
+    if AnimalTopPointer == 0 :
         return ''
     else:
-        remove = CircularQueue[Head]
-        Head = Head + 1
-        NumberOfItems = NumberOfItems - 1
-        if Head > 4:
-            Head = 0
-        print(CircularQueue[Head])
-        return CircularQueue[Head]
-def EnterRecord():
-    ID = input('enter an ID: ')
-    Quantity = int(input('enter quantity: '))
-    Data = SaleData(ID, Quantity)
-    Return = Enqueue(Data)
-    if Return == -1 :
-        print('full')
-    else:
-        print('stored')
+        ReturnData = Animal[AnimalTopPointer-1]
+        AnimalTopPointer = AnimalTopPointer -1
+        return ReturnData
 
-class Employee():
-    def __init__(self, pay, num, title):
-        self.HourlyPay = pay #REAL
-        self.EmployeeNumber = num #STRING
-        self.JobTitle = title #STRING
-        self.PayYear2022 = [0.0 for i in range(51)] #REAL
-    def GetEmployeeNumber(self):
-        return self.EmployeeNumber
-    def SetPay(self, weeknum, numhours):
-        total = weeknum * numhours
-        flag = False
-        i = 0
-        while flag == False:
-            if self.PayYear2022[i] == 0.0:
-                self.PayYear2022 = total
-                flag = True
-    def GetTotalPay(self):
-        for i in range(51):
-            while self.PayYear2022[i] != 0.0:
-                print(self.PayYear2022[i])
-                return self.PayYear2022
-class Manager(Employee):
-    def __init__(self, pay, num, val, title):
-        Employee.__init__(self, pay, num, title)
-        self.BonusValue = val #REAL
-    def SetPay(self, weeknum, numhours):
-        newhours = numhours + (numhours * self.BonusValue/100)
-        Employee.SetPay(self,weeknum,newhours)
-#main
-EmployeeArray = []
-try:
-    file = open('C:/Users/N129ALS22030/Desktop/Employees.txt', 'r')
-    i = 0
-    pay = file.readline()
-    num = file.readline()
-    bonus = file.readline()
-    if float(bonus) == float:
-        title = file.readline()
-        temp = Manager(pay,num,bonus,title)
-        EmployeeArray.append(temp)
+def PushColour(DataToPush):
+    global ColourTopPointer
+    if ColourTopPointer == 20:
+        return False
     else:
-        temp = Employee(pay,num,bonus)
-        EmployeeArray.append(temp)
-    file.close()
-except IOError:
-    print('file doesnt exist')
+        Colour[ColourTopPointer] = DataToPush
+        ColourTopPointer = ColourTopPointer + 1
+        return True
+def PopColour():
+    global ColourTopPointer
+    if ColourTopPointer == 0 :
+        return ''
+    else:
+        ReturnData = Animal[ColourTopPointer-1]
+        ColourTopPointer = ColourTopPointer -1
+        return ReturnData
 
-def EnterHours():
+def ReadData():
+    AnimalFile = 'C:/Users/N129ALS22030/Desktop/AnimalData.txt'
+    ColourFile = 'C:/Users/N129ALS22030/Desktop/ColourData.txt'
     try:
-        hoursfile = open('C:/Users/N129ALS22030/Desktop/HoursWeek1.txt', 'r')
-        num = hoursfile.readline()
-        for i in range(len(EmployeeArray)):
-            if num == EmployeeArray[i].GetEmployeeNumber():
-                hours = hoursfile.readline()
-                EmployeeArray[i].SetPay(EmployeeArray[i].HourlyPay, hours)
-        file.close()
+        File = open(AnimalFile, 'r')
+        File2 = open(ColourFile, 'r')
+        read = File.readline()
+        read2 = File2.readline()
+        PushAnimal(read)
+        PushColour(read2)
+        File.close()
+        File2.close()
     except IOError:
         print('file doesnt exist')
-#amend main
-EnterHours()
-for i in range(len(EmployeeArray)):
-    num = EmployeeArray[i].GetEmployeeNumber()
-    print(num)
-    TotalPay = EmployeeArray[i].GetTotalPay()
+
+def OutputItem():
+    Col = PopColour()
+    animal = PopAnimal()
+    if Col != '' and animal != '':
+        print(Col + animal)
+    elif Col == '':
+        PushAnimal(animal)
+        print('no colour')
+    elif animal == '':
+        PushColour(Col)
+        print('no animal')
+
+#main
+ReadData()
+OutputItem()
+OutputItem()
+OutputItem()
+OutputItem()
+class Vehicle():
+    def __init__(self,id,maxspeed,inc):
+        self.__ID = id #STRING
+        self.__MaxSpeed = maxspeed #INTEGER
+        self.__CurrentSpeed = 0 #INTEGER
+        self.__IncreaseAmount = inc #INTEGER
+        self.__HorizontalPosition = 0 #INTEGER
+    def GetCurrentSpeed(self):
+        return self.__CurrentSpeed
+    def GetIncreasseAmount(self):
+        return self.__IncreaseAmount
+    def GetHorizontalPosition(self):
+        return self.__HorizontalPosition
+    def GetMaxSpeed(self):
+        return self.__MaxSpeed
+    def SetCurrentSpeed(self, current):
+        self.__CurrentSpeed = current
+    def SetHorizontalPosition(self, hori):
+        self.__HorizontalPosition = hori
+    def IncreaseSpeed(self):
+        if self.__CurrentSpeed <= self.__MaxSpeed:
+            updatedspeed = self.__CurrentSpeed + self.__IncreaseAmount
+            self.__HorizontalPosition = updatedspeed
+
+class Helicopter(Vehicle):
+    def __init__(self,id,max,current,verchange,maxheight):
+        Vehicle.__init__(self,id,max,current)
+        self.__VerticalPosition = 0 #INTEGER
+        self.__VerticalChange = verchange #INTEGER
+        self.__MaxHeight = maxheight #INTEGER
+    def GetVerticalPosition(self):
+        return self.__VerticalPosition
+    def IncreaseSpeed(self):
+        Helicopter.GetVerticalPosition(self)
+        self.__VerticalPosition = self.__VerticalPosition + self.__VerticalChange
+        if self.__VerticalPosition <= self.__MaxHeight:
+            if Helicopter.GetCurrentSpeed(self) <= Helicopter.GetMaxSpeed(self):
+                updatedspeed = self.__CurrentSpeed + self.__IncreaseAmount
+                Helicopter.SetHorizontalPosition(self,updatedspeed)
+def output(Vehicle):
+    print(Vehicle.GetHorizontalPosition())
+    print(Vehicle.GetCurrentSpeed())
+    if Vehicle == 'Helicopter' :
+        print(Vehicle.GetVerticalPosition())
+
+#main
+car = Vehicle('Tiger', 100, 20)
+heli = Helicopter('Lion', 350, 40, 3, 100 )
+car.IncreaseSpeed()
+car.IncreaseSpeed()
+output(car)
+heli.IncreaseSpeed()
+heli.IncreaseSpeed()
+output(heli)
+
+
