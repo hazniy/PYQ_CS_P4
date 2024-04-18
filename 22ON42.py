c9a09948f59aba62065c73f51df21a799cb184ee
Farhazni Najwa <141621627+hazniy@users.noreply.github.com>
2024-04-18 11:50:17 +0800

    22ON42.py

--- 22ON42.py
+++ 22ON42.py
@@ -1,4 +1,40 @@
 #ques 1 insertion
+def Initialise():
+ global Jobs
+ global NumberOfJobs
+ for x in range(0, 100):
+ Jobs.append([-1,-1])
+ NumberOfJobs = 0
+
+def AddJob(JobNumber, Priority):
+ global NumberOfJobs
+ global Jobs
+ if NumberOfJobs == 100:
+ print("Not added")
+ else:
+ Jobs[NumberOfJobs] = [JobNumber, Priority]
+ print("Added")
+ NumberOfJobs = NumberOfJobs + 1
+
+def PrintArray():
+ global Jobs
+ global NumberOfJobs
+ for X in range(0, NumberOfJobs):
+ print(str(Jobs[X][0]), " priority ", str(Jobs[X][1]))
+
+def InsertionSort():
+ global Jobs
+ global NumberOfJobs
+ for I in range(1, NumberOfJobs):
+ Current1 = Jobs[I][0]
+ Current2 = Jobs[I][1]
+ while I > 0 and Jobs[I-1][1] > Current2:
+ Jobs[I][0] = Jobs[I-1][0]
+ Jobs[I][1] = Jobs[I-1][1]
+ I = I - 1
+ Jobs[I][0] = Current1
+ Jobs[I][1] = Current2
+
 #ques 2 OOP / Text File
 #(a)
 class Character:
